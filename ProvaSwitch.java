0e85a053d6c582cab7920fa3d8c4a1a9713cfd37
Giuseppe <giuseppe.fahmy@gmail.com>
2020-01-28 12:32:23 +0100

    Add files via upload

---
+++ ProvaSwitch.java
@@ -0,0 +1,33 @@
+public class ProvaSwitch{
+	
+
+	public static void main(String[] args) {
+		
+		int mese=5;
+
+		switch(mese)
+		{
+			case 1:
+				System.out.println("Gennaio");
+				break;
+			case 2:
+				System.out.println("Febbraio");
+				break;
+			case 3:
+				System.out.println("Marzo");
+				break;
+			case 4:
+				System.out.println("Aprile");
+				break;
+
+				default:
+					System.out.println("numero non valido");
+
+		}
+		
+		
+
+
+	}
+
+}
