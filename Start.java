fc45fe37d077a0bc40dadefb61f6e1ad93e94bb6
Giuseppe <giuseppe.fahmy@gmail.com>
2020-02-06 17:01:20 +0100

    Add files via upload

---
+++ Start.java
@@ -0,0 +1,16 @@
+import elis.amazon.*;
+import elis.amazon.categorie.Abbigliamento;
+public class Start
+{
+	
+	public static void main(String[] args) {
+		
+		PrimaAmazon x=new PrimaAmazon();
+		Abbigliamento y=new Abbigliamento();
+		elis.utility.CheckLogin z=new elis.utility.CheckLogin();
+
+	}
+
+
+
+}
