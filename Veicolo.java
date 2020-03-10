4c150aa51d75fa1cf8a4c14d395b08c93b259b62
capitanmirco <60383198+capitanmirco@users.noreply.github.com>
2020-03-10 14:47:05 +0100

    Add files via upload

--- Veicolo.java
+++ Veicolo.java
@@ -1,12 +1,42 @@
-public class Veicolo
-{
-	private String tipoMotore;
+package it.elis.java;
 
-
-
-	public void setMotore(String m)
-	{
-		this.tipoMotore=m;
-
+public class Veicolo {
+	
+	public String nome;
+	public String marca;
+	public double cilindrata;
+	public Veicolo(String nome, String marca, double cilindrata) {
+		super();
+		this.nome = nome;
+		this.marca = marca;
+		this.cilindrata = cilindrata;
 	}
+	
+	public String getNome() {
+		return this.nome;
+	}
+	public String getMarca() {
+		return this.marca;
+	}
+	public double getCilindrata() {
+		return this.cilindrata;
+	}
+	public void setNome(String nome) {
+		this.nome=nome;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public void setCilindrata(double cilindrata) {
+		this.cilindrata = cilindrata;
+	}
+	public String info(){
+		return this.marca+" "+" "+this.nome+" "+this.cilindrata;
+	}
+	public void accellera() {
+		System.out.println("Sto accellerando");
+	}
+	
+	
+	
 }
